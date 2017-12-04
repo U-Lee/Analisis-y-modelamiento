aa4a6da2bb096769ea62f365dcc787dd3020be7a
Billydulfo <33133495+Billydulfo@users.noreply.github.com>
2017-12-03 19:40:36 -0600

    modificación para guardar imagen en base datos
    Integre  codigo encargado de capturar la imagen

--- Aplicacion/src/controlador/ControladorGuardaCandidato.java
+++ Aplicacion/src/controlador/ControladorGuardaCandidato.java
@@ -14,6 +14,14 @@ import modelo.Grupo;
 import modelo.Tesis;
 import modelo.Trabajo;
 import vista.JPanelCreateCandidato;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -40,39 +48,55 @@ public class ControladorGuardaCandidato implements ActionListener{
     
     
     public void actionPerformed(ActionEvent e){
-        String matriculaEst;
-        
-        candidato.setNombre(jPanelCreateCandidato.jtxtNombre.getText());
-        candidato.setApellidoPaterno(jPanelCreateCandidato.jtxtApellidoPaterno.getText());
-        candidato.setApellidoMaterno(jPanelCreateCandidato.jtxtApellidoMaterno.getText());
-        candidato.setCorreoElectronico(jPanelCreateCandidato.jtxtCorrElectronico.getText());
-        candidato.setTelefono(jPanelCreateCandidato.jtxtTelefono.getText());
-        /*la captura de matricula se hace por partes*/
-        matriculaEst = jPanelCreateCandidato.jtxtAnioMatricula.getText() +
-                "-"+jPanelCreateCandidato.jtxtPlantelMatricula.getText()+
-                "-"+jPanelCreateCandidato.jtxtMatricula.getText();
-        
-        candidato.setMatricula(matriculaEst);
-        candidato.setGeneracion(jPanelCreateCandidato.jtxtGeneracion.getText());
-        candidato.setCreditos(jPanelCreateCandidato.jSpinnerCreditos.getToolTipText());/*anexado spinner credtidos 30/11/17*/
-        /*acciones para carrera*/
-        //carrera.setNombre(jPanelCreateCandidato.jComBoxCarrera.toString());/*aqui se carga la tabla carreras*/
-        candidato.setCarrera(carrera);
-        /*acciones para tesis*/
-        tesis.setTema(jPanelCreateCandidato.jtxtNomTesis.getText());
-        tesis.setDirector(jPanelCreateCandidato.jtxtDircTesis.getText());
-        candidato.setTesis(tesis);
-        /*acciones para trabajo*/
-        //trabajo.setNombreEntidad(jPanelCreateCandidato.jtxtEntidad.getText());
-        candidato.setTrabajo(trabajo);
-        /*falta incorporar obtencion de carta compromiso
-        *obtencion de carta de exposicion de motivos
-        *obtencion de foto del candidato, tambien subir foto
-        *captura de horarios y subir carreras desde postgresql*/
-        
-        /*agregamos a la base de datos*/
-        //grupo.setCandidato(candidato);
-        adminBD.insertarCandidato(candidato);
+            String matriculaEst;
+            candidato.setNombre(jPanelCreateCandidato.jtxtNombre.getText());
+            candidato.setApellidoPaterno(jPanelCreateCandidato.jtxtApellidoPaterno.getText());
+            candidato.setApellidoMaterno(jPanelCreateCandidato.jtxtApellidoMaterno.getText());
+            candidato.setCorreoElectronico(jPanelCreateCandidato.jtxtCorrElectronico.getText());
+            candidato.setTelefono(jPanelCreateCandidato.jtxtTelefono.getText());
+            /*la captura de matricula se hace por partes*/
+            matriculaEst = jPanelCreateCandidato.jtxtAnioMatricula.getText() +
+                    "-"+jPanelCreateCandidato.jtxtPlantelMatricula.getText()+
+                    "-"+jPanelCreateCandidato.jtxtMatricula.getText();
+            
+            candidato.setMatricula(matriculaEst);
+            candidato.setGeneracion(jPanelCreateCandidato.jtxtGeneracion.getText());
+            candidato.setCreditos(jPanelCreateCandidato.jSpinnerCreditos.getToolTipText());/*anexado spinner credtidos 30/11/17*/
+            /*acciones para carrera*/
+            //carrera.setNombre(jPanelCreateCandidato.jComBoxCarrera.toString());/*aqui se carga la tabla carreras*/
+            candidato.setCarrera(carrera);
+            /*acciones para tesis*/
+            tesis.setTema(jPanelCreateCandidato.jtxtNomTesis.getText());
+            tesis.setDirector(jPanelCreateCandidato.jtxtDircTesis.getText());
+            candidato.setTesis(tesis);
+            /*acciones para trabajo*/
+            //trabajo.setNombreEntidad(jPanelCreateCandidato.jtxtEntidad.getText());
+            candidato.setTrabajo(trabajo);
+            try{
+                candidato.setFoto(extractBytes(jPanelCreateCandidato.fichero));
+            }catch (IOException ex) {
+                Logger.getLogger(ControladorGuardaCandidato.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            
+            /*falta incorporar obtencion de carta compromiso
+            *obtencion de carta de exposicion de motivos
+            *obtencion de foto del candidato, tambien subir foto
+            *captura de horarios y subir carreras desde postgresql*/
+            
+            /*agregamos a la base de datos*/
+            adminBD.insertarCandidato(candidato);
+       
     }
-    
+        /*bloque para convertir imagen a bytea*/
+    public byte[] extractBytes (File imgPath) throws IOException {
+    // open image
+        BufferedImage bufferedImage = ImageIO.read(imgPath);
+
+    // get DataBufferBytes from Raster
+        WritableRaster raster = bufferedImage .getRaster();
+        DataBufferByte data   = (DataBufferByte) raster.getDataBuffer();
+
+        return ( data.getData() );
+    }
 }
