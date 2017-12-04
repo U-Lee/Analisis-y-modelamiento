c3b99583776684c001282f9f4f394978bb696c56
Oswaldo UACM <Oswaldo UACM@DESKTOP-71G9K8T>
2017-12-04 00:38:20 -0600

    Check style

--- Aplicacion/src/controlador/ControladorGuardaAsesor.java
+++ Aplicacion/src/controlador/ControladorGuardaAsesor.java
@@ -1,22 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import modelo.AdminBD;
 import modelo.Asesor;
 import modelo.Horario;
 import vista.JPanelCreateAsesor;
-
-/**
- *
- * @author Jhon Nash
- */
-public class ControladorGuardaAsesor implements ActionListener{
+    /**
+    * Controlador que guarda datos del acesor en la base de datos.
+    * @author - Quality Developers and Software
+    * @version - Segundo Sprint
+    * @see - Controlador guarda la en la base datos
+    * @param - Asesor variable de tipo asesor
+    * @param - Horario varible de tipo Horario, definira el horario de asesorias
+    * @param - AdminBD que almacenara en la base de datos
+    *
+    */
+public class ControladorGuardaAsesor implements ActionListener {
     private Asesor asesor;
     private Horario horario;
     private AdminBD adminBD;
@@ -26,20 +25,25 @@ public class ControladorGuardaAsesor implements ActionListener{
         horario = new Horario();
         adminBD = new AdminBD();
         asesor = new Asesor();
-        
         this.jPanelCreateAsesor = jPanelCreateAsesor;
         jPanelCreateAsesor.jbttnGuardar.addActionListener(this);
     }
-    
-    public void actionPerformed(ActionEvent e){
+    /*
+    *Evento del boton para obtener datos de la vista
+    */
+    public void actionPerformed(ActionEvent e) {
         asesor.setNombre(jPanelCreateAsesor.jtxtNombreAsesor.getText());
-        asesor.setApellidoPaterno(jPanelCreateAsesor.jtxtApellidoPaterno.getText());
-        asesor.setApellidoMaterno(jPanelCreateAsesor.jtxtApellidoMaterno.getText());
-        asesor.setCorreoElectronico(jPanelCreateAsesor.jtxtCorrEleAsesor.getText());
-        asesor.setTelefono(jPanelCreateAsesor.jtxtTelefono.getText());
-        asesor.setCubiculo(jPanelCreateAsesor.jtxtCubiculo.getText());
+        asesor.setApellidoPaterno(
+                jPanelCreateAsesor.jtxtApellidoPaterno.getText());
+        asesor.setApellidoMaterno(
+                jPanelCreateAsesor.jtxtApellidoMaterno.getText());
+        asesor.setCorreoElectronico(
+                jPanelCreateAsesor.jtxtCorrEleAsesor.getText());
+        asesor.setTelefono(
+                jPanelCreateAsesor.jtxtTelefono.getText());
+        asesor.setCubiculo(
+                jPanelCreateAsesor.jtxtCubiculo.getText());
         /*guardamos en la bd17*/
-        adminBD.insertarAsesor(asesor);       
+        adminBD.insertarAsesor(asesor);
     }
-  
 }
